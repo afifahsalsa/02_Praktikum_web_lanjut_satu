37bed4b24b25b3413b6e70d241b8d8007bc0f9cf
= <afifahsalsa.billa2@gmail.com>
2023-02-26 18:31:11 +0700

    Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Node\Stmt\Echo_;
 
@@ -30,6 +31,50 @@ Route::get('/', function () {
 });
 */
 
+//nomor 1
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/about', [AboutController::class, 'about']);
-Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+//nomor 2
+Route::prefix('product')->group(function(){
+    Route::get('/make-up', function(){
+        return 'Daftar Make-up yang Masih Tersedia';
+    });
+    Route::get('/baju-anak', function(){
+        return 'Baju Anak yang masih Ready';
+    });
+    Route::get('/peralatan-mandi', function(){
+        return 'Ini daftar peralatan mandi yang ready';
+    });
+    Route::get('/baju-dewasa', function(){
+        return 'Baju Dewasa';
+    }); 
+});
+
+//nomor 3
+Route::get('/news/{name?}', function ($name = 'Minggu'){
+    return 'Air Hujan Telah Mengguyur Kota Malang Pada Hari ' .$name;
+});
+Route::get('/news/{name}/cuaca-hari-ini/{comment}', function ($name, $comment){
+    return 'Air Hujan Telah Mengguyur Kota Malang Pada Hari ' .$name . '<br> Dengan Perkiraan Akan disertai ' .$comment;
+});
+
+//nomor 4
+Route::prefix('Kampus-Merdeka')->name('program') ->group(function(){
+    Route::get('/MSIB', function(){
+        return 'Daftar Program yang masih tersedia';
+    });
+    Route::get('/Magang', function(){
+        return 'Lowongan Perusahaan ';
+    });
+    Route::get('/IISMA', function(){
+        return 'Daftar Program yang Masih Tersedia';
+    });
+});
+
+//nomor 5
+Route::get('/about-us', function(){
+    return 'This is Page About Us';
+});
+
+//nomor 6
+Route::resource('photos', PhotoController::class)->only(['index', 'show']);
